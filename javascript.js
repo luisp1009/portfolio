d4a7bdc9f6c5e5f5790ec701ec782f1aa95a0ad1
LuisParedes <luisparedes1009@hotmail.com>
2024-04-05 14:39:07 -0400

    projects updates

--- javascript.js
+++ javascript.js
@@ -17,7 +17,9 @@ document.addEventListener("scroll", () => {
 
     if (
       targetElement &&
-      scrollPosition >= targetElement.offsetTop - document.querySelector('.navbar').offsetHeight &&
+      scrollPosition >=
+        targetElement.offsetTop -
+          document.querySelector(".navbar").offsetHeight &&
       scrollPosition < targetElement.offsetTop + targetElement.offsetHeight
     ) {
       navLinks.forEach((link) => link.classList.remove("active"));
@@ -26,29 +28,25 @@ document.addEventListener("scroll", () => {
   });
 });
 
-
 window.addEventListener("scroll", function () {
-   var navbar = document.getElementById("navbar");
-   if (window.scrollY > 20) {
-     navbar.classList.add("scrolled");
-   } else {
-     navbar.classList.remove("scrolled");
-   }
- });
-
- window.addEventListener('scroll', function() {
-  var logo = document.querySelector('.logo');
-  var logoLink = document.getElementById('logo-link');
+  var navbar = document.getElementById("navbar");
   if (window.scrollY > 20) {
-    logo.style.width = '65px';
-   
+    navbar.classList.add("scrolled");
   } else {
-    logo.style.width = '120px';
-    logoLink.style.padding = '0';
+    navbar.classList.remove("scrolled");
   }
 });
 
-
+window.addEventListener("scroll", function () {
+  var logo = document.querySelector(".logo");
+  var logoLink = document.getElementById("logo-link");
+  if (window.scrollY > 20) {
+    logo.style.width = "65px";
+  } else {
+    logo.style.width = "120px";
+    logoLink.style.padding = "0";
+  }
+});
 
 // Add this to javascript.js
 
@@ -62,29 +60,83 @@ document.addEventListener("DOMContentLoaded", function () {
   darkModeToggle.checked = isDarkMode;
   body.classList.toggle("dark-mode", isDarkMode);
 
- 
   // Toggle dark mode on switch change
   darkModeToggle.addEventListener("change", function () {
     const isDarkMode = darkModeToggle.checked;
     body.classList.toggle("dark-mode", isDarkMode);
     localStorage.setItem("darkMode", isDarkMode);
-
   });
 });
 
-
-document.body.onmousemove = function(e) {
-  document.documentElement.style.setProperty (
-    '--x', (
-      e.clientX+window.scrollX
-    )
-    + 'px'
+document.body.onmousemove = function (e) {
+  document.documentElement.style.setProperty(
+    "--x",
+    e.clientX + window.scrollX + "px"
   );
-  document.documentElement.style.setProperty (
-    '--y', (
-      e.clientY+window.scrollY
-    ) 
-    + 'px'
+  document.documentElement.style.setProperty(
+    "--y",
+    e.clientY + window.scrollY + "px"
   );
-}
+};
 
+document.addEventListener("DOMContentLoaded", function() {
+  const galleryContainer = document.querySelector(".gallery-container");
+  const galleryControlsContainer = document.querySelector(".gallery-controls");
+  const galleryControls = ["previous", "next"];
+  const galleryItems = document.querySelectorAll(".gallery-item");
+
+  class Carousel {
+    constructor(container, items, controls) {
+      this.carouselContainer = container;
+      this.carouselControls = controls;
+      this.carouselArray = [...items];
+    }
+
+    updateGallery() {
+      this.carouselArray.forEach((el) => {
+        el.classList.remove("gallery-item-1", "gallery-item-2", "gallery-item-3", "gallery-item-4", "gallery-item-5");
+      });
+
+      this.carouselArray.slice(0, 5).forEach((el, i) => {
+        el.classList.add(`gallery-item-${i + 1}`);
+      });
+    }
+
+    setCurrentState(direction) {
+      if (direction.className === "gallery-controls-previous") {
+        this.carouselArray.unshift(this.carouselArray.pop());
+      } else {
+        this.carouselArray.push(this.carouselArray.shift());
+      }
+      this.updateGallery();
+    }
+
+    setControls() {
+      this.carouselControls.forEach((control) => {
+        const button = document.createElement("button");
+        button.className = `gallery-controls-${control}`;
+        button.innerText = control;
+        galleryControlsContainer.appendChild(button);
+      });
+    }
+
+    useControls() {
+      const triggers = [...galleryControlsContainer.querySelectorAll("button")];
+      triggers.forEach((control) => {
+        control.addEventListener("click", (e) => {
+          e.preventDefault();
+          this.setCurrentState(control);
+        });
+      });
+    }
+  }
+
+  const exampleCarousel = new Carousel(
+    galleryContainer,
+    galleryItems,
+    galleryControls
+  );
+
+  exampleCarousel.setControls();
+  exampleCarousel.useControls();
+});
