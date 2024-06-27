4b60c71fa3ebdbc38066df1b28e87bb48da8f42c
LuisParedes <luisparedes1009@hotmail.com>
2024-06-27 17:42:37 -0400

    dark/light mode

--- javascript.js
+++ javascript.js
@@ -28,25 +28,25 @@ document.addEventListener("scroll", () => {
   });
 });
 
-window.addEventListener("scroll", function () {
-  var navbar = document.getElementById("navbar");
-  if (window.scrollY > 20) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
+// window.addEventListener("scroll", function () {
+//   var navbar = document.getElementById("navbar");
+//   if (window.scrollY > 20) {
+//     navbar.classList.add("scrolled");
+//   } else {
+//     navbar.classList.remove("scrolled");
+//   }
+// });
 
-window.addEventListener("scroll", function () {
-  var logo = document.querySelector(".logo");
-  var logoLink = document.getElementById("logo-link");
-  if (window.scrollY > 20) {
-    logo.style.width = "65px";
-  } else {
-    logo.style.width = "120px";
-    logoLink.style.padding = "0";
-  }
-});
+// window.addEventListener("scroll", function () {
+//   var logo = document.querySelector(".logo");
+//   var logoLink = document.getElementById("logo-link");
+//   if (window.scrollY > 20) {
+//     logo.style.width = "65px";
+//   } else {
+//     logo.style.width = "120px";
+//     logoLink.style.padding = "0";
+//   }
+// });
 
 // Add this to javascript.js
 
@@ -68,75 +68,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-document.body.onmousemove = function (e) {
-  document.documentElement.style.setProperty(
-    "--x",
-    e.clientX + window.scrollX + "px"
-  );
-  document.documentElement.style.setProperty(
-    "--y",
-    e.clientY + window.scrollY + "px"
-  );
-};
 
-document.addEventListener("DOMContentLoaded", function() {
-  const galleryContainer = document.querySelector(".gallery-container");
-  const galleryControlsContainer = document.querySelector(".gallery-controls");
-  const galleryControls = ["previous", "next"];
-  const galleryItems = document.querySelectorAll(".gallery-item");
-
-  class Carousel {
-    constructor(container, items, controls) {
-      this.carouselContainer = container;
-      this.carouselControls = controls;
-      this.carouselArray = [...items];
-    }
-
-    updateGallery() {
-      this.carouselArray.forEach((el) => {
-        el.classList.remove("gallery-item-1", "gallery-item-2", "gallery-item-3", "gallery-item-4", "gallery-item-5");
-      });
-
-      this.carouselArray.slice(0, 5).forEach((el, i) => {
-        el.classList.add(`gallery-item-${i + 1}`);
-      });
-    }
-
-    setCurrentState(direction) {
-      if (direction.className === "gallery-controls-previous") {
-        this.carouselArray.unshift(this.carouselArray.pop());
-      } else {
-        this.carouselArray.push(this.carouselArray.shift());
-      }
-      this.updateGallery();
-    }
-
-    setControls() {
-      this.carouselControls.forEach((control) => {
-        const button = document.createElement("button");
-        button.className = `gallery-controls-${control}`;
-        button.innerText = control;
-        galleryControlsContainer.appendChild(button);
-      });
-    }
-
-    useControls() {
-      const triggers = [...galleryControlsContainer.querySelectorAll("button")];
-      triggers.forEach((control) => {
-        control.addEventListener("click", (e) => {
-          e.preventDefault();
-          this.setCurrentState(control);
-        });
-      });
-    }
-  }
-
-  const exampleCarousel = new Carousel(
-    galleryContainer,
-    galleryItems,
-    galleryControls
-  );
-
-  exampleCarousel.setControls();
-  exampleCarousel.useControls();
-});
